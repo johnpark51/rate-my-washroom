4ea2f1d7c809d77199fcc7dfed835eff78e71051
John Park <johnparkbluejay@gmail.com>
2024-06-25 10:56:08 -0400

    colour palette added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,13 +2,15 @@ import "./Header.scss";
 
 function Header() {
 	return (
-		<header className="header">
-			<h2 className="header__title">Rate My Washroom</h2>
-			<nav className="header__right">
-				<button className="header__button">Sign up</button>
-				<button className="header__button">Login</button>
-			</nav>
-		</header>
+		<div id="container">
+			<header className="header">
+				<h2 className="header__title">Rate My Washroom</h2>
+				<nav className="header__right">
+					<button className="header__button header__button--signup">Sign up</button>
+					<button className="header__button header__button--login">Login</button>
+				</nav>
+			</header>
+		</div>
 	);
 }
 
