6fe6d82b04a9a840fa4eb2e8aff755b1f8083ed8
John Park <johnparkbluejay@gmail.com>
2024-06-25 14:48:27 -0400

    homepage hero - hero img and main header&input added

--- client/src/components/HomePageHero/HomePageHero.jsx
+++ client/src/components/HomePageHero/HomePageHero.jsx
@@ -1,11 +1,18 @@
 import "./HomePageHero.scss";
+// import { washroom } from "../../assets/images/washroom.jpg"
 
 function HomePageHero() {
 	return (
 		<div id="container">
-            <section>
-                <h1>Find the right washroom for you</h1>
-            </section>
+			<section className="hero">
+				<div className="hero__overlay">
+					<h1 className="hero__header">Find the right <span className="hero__washroom">washroom</span> for you</h1>
+					<input placeholder="Search your area or scroll below" className="hero__input"></input>
+                    <div className="hero__location-section">
+                        <p>hello world</p>
+                    </div>
+				</div>
+			</section>
 		</div>
 	);
 }
