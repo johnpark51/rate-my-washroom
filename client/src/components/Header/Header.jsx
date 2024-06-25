25a87c229cfc627576b90fdbdd1292f8e5993cb8
John Park <johnparkbluejay@gmail.com>
2024-06-25 10:42:01 -0400

    header structure built

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
-import "./Header.scss"
+import "./Header.scss";
 
 function Header() {
-    return (
-        <>
-        <h1>Rate My Washroom</h1>
-        </>
-    )
+	return (
+		<header className="header">
+			<h2 className="header__title">Rate My Washroom</h2>
+			<nav className="header__right">
+				<button className="header__button">Sign up</button>
+				<button className="header__button">Login</button>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
