c563536f4cb785db464d1c8adcc31d2868b4a8f1
John Park <johnparkbluejay@gmail.com>
2024-06-25 12:04:19 -0400

    header - link added to header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import "./Header.scss";
 
+import { Link } from "react-router-dom"
+
 function Header() {
 	return (
 		<div id="container">
 			<header className="header">
-				<h2 className="header__title">RateMy<span className="header__washroom">Washroom</span></h2>
+				<Link className="link" to="/"><h2 className="header__title">RateMy<span className="header__washroom">Washroom</span></h2></Link>
 				<nav className="header__right">
 					<button className="button button--login">Login</button>
 					<button className="button button--signup">Sign up</button>
