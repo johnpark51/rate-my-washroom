2b0b7a7b53c37977f294ade0acbb35d6a25e0339
John Park <johnparkbluejay@gmail.com>
2024-06-26 15:15:06 -0400

    re-downloading files to try to fix seed file execution error

---
+++ server/seed-data/reviewsData.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        id: 1,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        washroom_id: 1,
+    },
+    {
+        id: 2,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 2,
+    },
+    {
+        id: 3,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 2,
+    },
+    {
+        id: 4,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 3,
+    },
+    {
+        id: 5,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 3,
+    },
+    {
+        id: 6,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 3,
+    },
+    {
+        id: 7,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 4,
+    },
+    {
+        id: 8,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 4,
+    },
+    {
+        id: 9,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 4,
+    },
+    {
+        id: 10,
+        name: "John Doe",
+        content: "Customer service was very friendly in assisting me to where the washroom was, even tough I wan't a customer. Toiler paper was 3 ply and washroom was very clean. Great experience",
+        // created_at: 1719417780,
+        washroom_id: 4,
+    },
+    
+]
