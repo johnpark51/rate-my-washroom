3e5f064d4bab8d8a888dbf3f6470cde329069574
John Park <johnparkbluejay@gmail.com>
2024-06-27 09:34:56 -0400

    nodemon added

--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5050;
 
+import routes from "./routes/routes.js"
+app.use("/", routes);
+
 // basic home route
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
 });
 
 app.listen(PORT, () => {
-  console.log(`running at http://localhost:${PORT}`);
+  console.log(`Running at http://localhost:${PORT}`);
 });
