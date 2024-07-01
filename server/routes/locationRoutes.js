db71671ed2e5d5e13a16baa7dbbb2b34b33ebb01
John Park <johnparkbluejay@gmail.com>
2024-07-01 18:59:26 -0400

    services - washroom knex get requests complete

--- server/routes/locationRoutes.js
+++ server/routes/locationRoutes.js
@@ -12,31 +12,6 @@ router
   .route("/:id")
   .get(fetchOne);
 
-// router.get("/locations", async (_req, res) => {
-//   try {
-//     const data = await knex("locations");
-//     res.status(200).json(data);
-//   } catch (err) {
-//     res.status(400).send(`Error retrieving Users: ${err}`);
-//   }
-// });
 
-// router.get("/washrooms", async (_req, res) => {
-//     try {
-//       const data = await knex("washrooms");
-//       res.status(200).json(data);
-//     } catch (err) {
-//       res.status(400).send(`Error retrieving Users: ${err}`);
-//     }
-//   });
-
-//   router.get("/reviews", async (_req, res) => {
-//     try {
-//       const data = await knex("reviews");
-//       res.status(200).json(data);
-//     } catch (err) {
-//       res.status(400).send(`Error retrieving Users: ${err}`);
-//     }
-//   });
 
 export default router;
