e1352cd7e02ef84f1a6f4913acc48d3dc2776438
John Park <johnparkbluejay@gmail.com>
2024-07-03 09:26:34 -0400

    side component - star rating added

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -4,9 +4,12 @@ import "./HomePage.scss";
 import axios from "axios";
 import LocationList from "../../components/LocationList/LocationList";
 import About from "../../components/About/About";
+import { FaStar } from "react-icons/fa";
 
 function HomePage() {
 	const [locations, setLocations] = useState([]);
+	const [rating, setRating] = useState(null);
+	const [hover, setHover] = useState(null);
 
 	const baseURL = import.meta.env.VITE_API_URL;
 
@@ -21,6 +24,7 @@ function HomePage() {
 
 	useEffect(() => {
 		getLocations();
+		setRating(3)
 	}, []);
 
 	return (
@@ -28,9 +32,50 @@ function HomePage() {
 			<main>
 				<HomePageHero locations={locations} />
 				<LocationList locations={locations} />
-				{/* <img src="http://localhost:8080/images/stock1.jpg"/> */}
-				{/* <p>Hello world</p> */}
 				<About />
+				{[...Array(5)].map((star, index) => {
+					const currentRating = index + 1;
+					return (
+						<label>
+							<input
+								type="radio"
+								name="rating"
+								value={currentRating}
+								onClick={() => setRating(currentRating)}
+							/>
+							<FaStar
+								className="star"
+								size={50}
+								color={
+									currentRating <= (hover || rating) ? "ffc107" : "#e4e5e9"
+								}
+								onMouseEnter={() => setHover(currentRating)}
+								onMouseLeave={() => setHover(null)}
+							/>
+						</label>
+					);
+				})}
+				<p>Your rating is {rating}</p>
+				{[...Array(5)].map((star, index) => {
+					const currentRating = index + 1;
+					return (
+						<label>
+							<input
+								type="radio"
+								name="rating"
+								value={currentRating}
+							/>
+							<FaStar
+								className="star"
+								size={50}
+								color={
+									currentRating <= (rating) ? "ffc107" : "#e4e5e9"
+								}
+							/>
+						</label>
+					);
+				})}
+				<p>consistent rating</p>
 			</main>
 		</>
 	);
