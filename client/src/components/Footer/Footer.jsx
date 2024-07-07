1c4e53b676bd083b1bb672a03df291dd7a205f47
John Park <johnparkbluejay@gmail.com>
2024-07-07 16:25:20 -0400

    footer - component complete with styling done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,33 @@
-import "./Footer.scss"
+import "./Footer.scss";
+import { Link } from "react-router-dom";
+import { CiInstagram, CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
-    return (
-        <footer className="footer">
-            <div className="footer__container">
-                
-            </div>
-        </footer>
-    )
+	return (
+		<footer className="footer">
+			<div className="footer__container">
+				<Link className="links" to="/">
+					<h2 className="footer__header">
+						RateMy<span className="footer__washroom">Washroom</span>
+					</h2>
+				</Link>
+				<nav className="footer__nav">
+					<ul className="footer__ul">
+						<li className="footer__li">Home</li>
+						<li className="footer__li">Washrooms</li>
+						<li className="footer__li">Reviews</li>
+						<li className="footer__li">About</li>
+					</ul>
+				</nav>
+				<div className="footer__icons">
+                    <h3>Follow Me</h3>
+					<Link to="https://www.instagram.com/tkddnp/"><CiInstagram className="footer__icon"/></Link>
+					<Link to="https://www.linkedin.com/in/john-parkk/"><CiLinkedin className="footer__icon"/></Link>
+					<Link to="https://github.com/johnpark51?tab=repositories&ocid=AIDcmmli8vlwie_SEM__k_CjwKCAjw3NyxBhBmEiwAyofDYRBONd-u0dQiwFx9PCjeadYD6tOuAhzc9kfaTQbv8TiUDO9Onv9SWxoC-RgQAvD_BwE_k_"><FaGithub className="footer__icon"/></Link>
+				</div>
+                {/* <p>All Right Reserved</p> */}
+			</div>
+		</footer>
+	);
 }
--- client/src/components/HomeReviews/HomeReviews.jsx
+++ client/src/components/HomeReviews/HomeReviews.jsx
@@ -7,6 +7,7 @@ export default function HomeReviews({ review }) {
 		<section className="home-reviews">
 			<h3 className="home-reviews__name">{name}</h3>
 			<p className="home-reviews__address">{address}</p>
+			<p className="home-reviews__address home-reviews__location">{location}</p>
 			<Star rating={rating} className="home-reviews__rating"/>
 			<p className="home-reviews__content">{content}</p>
 		</section>
