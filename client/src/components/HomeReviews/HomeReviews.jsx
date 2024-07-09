c020d55c2c27d8955d421b1eab896c50498cb5d1
John Park <johnparkbluejay@gmail.com>
2024-07-09 19:32:43 -0400

    home reviews cards and section components reviewed and cleaned

--- client/src/components/HomeReviews/HomeReviews.jsx
+++ client/src/components/HomeReviews/HomeReviews.jsx
@@ -1,15 +1,18 @@
-import Star from "../Star/Star";
+/* STYLES */
 import "./HomeReviews.scss";
 
+/* COMPONENTS */
+import Star from "../Star/Star";
+
 export default function HomeReviews({ review }) {
-	const { name, content, rating, location, address, timestamp } = review;
+	const { name, content, rating, location, address } = review;
 	return (
-		<section className="home-reviews">
+		<card className="home-reviews">
 			<h3 className="home-reviews__name">{name}</h3>
 			<p className="home-reviews__address">{address}</p>
 			<p className="home-reviews__address home-reviews__location">{location}</p>
-			<Star rating={rating} className="home-reviews__rating"/>
+			<Star rating={rating} className="home-reviews__rating" />
 			<p className="home-reviews__content">{content}</p>
-		</section>
+		</card>
 	);
 }
--- client/src/components/HomeReviewsSection/HomeReviewsSection.jsx
+++ client/src/components/HomeReviewsSection/HomeReviewsSection.jsx
@@ -1,25 +1,25 @@
+/* STYLES */
 import "./HomeReviewsSection.scss";
 
+/* COMPONENTS */
 import HomeReviews from "../HomeReviews/HomeReviews";
 
 export default function HomeReviewsSection({ reviews }) {
 	return (
-		<section className="reviews-section" id="reviews">
-			<div className="reviews-section__container">
-				<div className="reviews-section__left">
-					<h2 className="reviews-section__header">Write a Review</h2>
-					<p className="reviews-section__description">
-						With direct inspiration from RateMyProf, we want to provide a
-						platform where you can share your honest review of a restroom you've
-						used to enhance the experience for others, or steer users away from a
-						restroom to benefit the community.
-					</p>
-				</div>
-				<div className="reviews-section__right">
-					{reviews.map((review, index) => {
-						return <HomeReviews key={index} review={review} />;
-					})}
-				</div>
+		<section className="reviews-section">
+			<div className="reviews-section__left">
+				<h2 className="reviews-section__header">Write a Review</h2>
+				<p className="reviews-section__description">
+					With direct inspiration from RateMyProf, we want to provide a platform
+					where you can share your honest review of a restroom you've used to
+					enhance the experience for others, or steer users away from a restroom
+					to benefit the community.
+				</p>
+			</div>
+			<div className="reviews-section__right">
+				{reviews.map((review, index) => {
+					return <HomeReviews key={index} review={review} />;
+				})}
 			</div>
 		</section>
 	);
