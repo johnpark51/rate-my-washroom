0902fafae45ab52aa71cba788c1b3a05642e5b93
John Park <johnparkbluejay@gmail.com>
2024-07-09 22:05:06 -0400

    washroom hero component reviewed and cleaned

--- client/src/components/WashroomHero/WashroomHero.jsx
+++ client/src/components/WashroomHero/WashroomHero.jsx
@@ -1,16 +1,22 @@
+/* STYLES */
 import "./WashroomHero.scss";
+
+/* ICONS */
 import { FaLocationDot } from "react-icons/fa6";
 
 export default function WashroomHero({ washroomDetails }) {
 	const { name, image } = washroomDetails;
+
 	return (
 		<section
-			className="washroom-hero"
+			className="hero washroom-hero"
 			style={{
 				backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7), rgb(0, 0, 0)), url(${image})`,
 			}}>
-			<FaLocationDot className="washroom-hero__location-icon" />
-			<h2 className="washroom-hero__header">{name}</h2>
+			<div className="washroom-hero__text">
+				<FaLocationDot className="washroom-hero__location-icon" />
+				<h2 className="washroom-hero__header">{name}</h2>
+			</div>
 		</section>
 	);
 }
