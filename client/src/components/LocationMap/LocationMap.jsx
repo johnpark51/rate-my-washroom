b678559b0b1f19e4fdeb4bb5421db97d6df6d456
John Park <johnparkbluejay@gmail.com>
2024-07-09 21:11:03 -0400

    location map component reviewed and cleaned

--- client/src/components/LocationMap/LocationMap.jsx
+++ client/src/components/LocationMap/LocationMap.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+/* STYLES */
 import "./LocationMap.scss";
+
+/* GOOGLE PACKAGE */
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
+/* COMPONENTS */
 import Point from "../Point/Point";
 
-export default function LocationMap({ washrooms, zoom, locationDetails }) {
+export default function LocationMap({ washrooms, zoom }) {
 	const markerData = washrooms.map((washroom) => {
 		let latFloat = parseFloat(washroom.lat)
 		let lngFloat = parseFloat(washroom.lng)
@@ -19,14 +23,12 @@ export default function LocationMap({ washrooms, zoom, locationDetails }) {
 		return <p>Loading...</p>;
 	}
 
-	console.log(washrooms)
-
 	return (
 		<APIProvider apiKey={"AIzaSyBFDj6opA7KGa4RfLButOzAcaqbCqJ1JmA"}>
 			<div className="location-map-div">
 				<Map defaultZoom={zoom} defaultCenter={markerData[0]} mapId={"8e6688f3b9182873"}>
 					{markerData.map((position) => {
-						return <Point position={position} />;
+						return <Point position={position} key={position.id}/>;
 					})}
 				</Map>
 			</div>
