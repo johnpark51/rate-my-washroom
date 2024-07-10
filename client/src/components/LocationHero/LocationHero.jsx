fbce6c56161cde53e8f625eeed27aeed9239325d
John Park <johnparkbluejay@gmail.com>
2024-07-09 20:47:21 -0400

    location hero component reviewed and cleaned

--- client/src/components/LocationHero/LocationHero.jsx
+++ client/src/components/LocationHero/LocationHero.jsx
@@ -1,17 +1,21 @@
+/* STYLES */
 import "./LocationHero.scss";
+
+/* ICONS */
 import { FaLocationDot } from "react-icons/fa6";
 
 export default function LocationHero({ locationDetails }) {
 	const { name, city, image } = locationDetails;
+
 	return (
 		<section
-			className="location-hero"
+			className="hero"
 			style={{
 				backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7), rgb(0, 0, 0)), url(${image})`,
 			}}>
 			<h2 className="location-hero__header">{name}</h2>
 			<div className="location-hero__right">
-                <FaLocationDot className="location-hero__location-icon"/>
+				<FaLocationDot className="location-hero__location-icon" />
 				<h4 className="location-hero__city">{city}</h4>
 			</div>
 		</section>
